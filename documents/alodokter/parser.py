c3e71e41f5d2a5435d0bc926a92f5e0fa420f2ce
reky <ardzz@indoxploit.or.id>
2023-05-11 14:34:03 +0700

    add `sanitize_dict`, `sanitize_text` and implement multi thread

--- documents/alodokter/parser.py
+++ documents/alodokter/parser.py
@@ -1,6 +1,25 @@
 from bs4 import BeautifulSoup
 import concurrent.futures
 from . import request
+from .sanitazer import Sanitizer
+
+
+def sanitize_dict(data: dict):
+    return {
+        "url_path": data["url_path"],
+        "image_url": data["image_url"],
+        "category": data["category"],
+        "title": sanitize_text(data["title"]),
+        "short_description": sanitize_text(data["short_description"]),
+        "article": sanitize_text(data["article"]),
+    }
+
+
+def sanitize_text(text: str):
+    if text is None:
+        return ""
+    else:
+        return Sanitizer(text).sanitize()
 
 
 def search2dict(html: str):
@@ -10,7 +29,7 @@ def search2dict(html: str):
 
     results = []
     for card_post in card_posts:
-        url_path =  "https://www.alodokter.com" + card_post["url-path"]
+        url_path = "https://www.alodokter.com" + card_post["url-path"]
         if "e-book-covid-19" not in url_path:
             results.append({
                 "url_path": url_path,
@@ -19,7 +38,6 @@ def search2dict(html: str):
                 "title": card_post["title"],
                 "short_description": card_post["short-description"].replace("...", ""),
             })
-
     pool = concurrent.futures.ThreadPoolExecutor(max_workers=30)
     futures = []
     for result in results:
@@ -28,10 +46,16 @@ def search2dict(html: str):
     for index, future in enumerate(futures):
         results[index]["article"] = future.result()
 
+    futures_sanitizer = []
+    for result in results:
+        futures_sanitizer.append(pool.submit(sanitize_dict, result))
+    concurrent.futures.wait(futures_sanitizer)
+    for index, future in enumerate(futures_sanitizer):
+        results[index] = future.result()
+
     return results
 
 
 def parse_article(html: str):
     soup = BeautifulSoup(html, "html.parser")
     return soup.find("div", {"class": "post-content"}).get_text()
-
