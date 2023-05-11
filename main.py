b83b88411513d553a612ff83832e08134bc65f84
reky <ardzz@indoxploit.or.id>
2023-05-11 14:43:34 +0700

    add sanitizer text

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ import documents.alodokter.request as alodokter_request
 from json import dumps
 import pandas as pd
 
-search = alodokter_request.search("covid 19")
-
-df = pd.DataFrame(search)
-df
+search = alodokter_request.search("covid 19", max_result=20)
+print(search)
